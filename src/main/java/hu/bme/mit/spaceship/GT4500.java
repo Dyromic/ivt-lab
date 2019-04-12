bf87a55521d59b3fcaaea3cde7e26aac7091b752
Dyromic <programozo007@gmail.com>
2019-04-12 09:14:44 +0200

    Comment added to success flag.

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -36,6 +36,7 @@ public class GT4500 implements SpaceShip {
   @Override
   public boolean fireTorpedo(FiringMode firingMode) {
 
+    // Success flag 
     boolean firingSuccess = false;
 
     switch (firingMode) {
